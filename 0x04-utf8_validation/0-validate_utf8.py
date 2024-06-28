793a08c01cf9fb67e6c4bcb6913353d725907935
Drihmia <drihmia.redouane@gmail.com>
2024-06-28 23:09:36 +0100

    fix: taking only the 8 least significant bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,21 +19,37 @@ def validUTF8(data: List[int]) -> bool:
     remaining_bytes = 0
 
     for num in data:
+        # Mask to get last 8 bits to get the 8 least significant bits
+        num &= 0xFF
+
         if remaining_bytes == 0:
+            # if the 8th bit is 0, then the number is a single byte character
             if (num >> 7) == 0:
                 continue
 
-            if (num >> 5) == 0b110:
+            # if the leftmost 3 bits are 110,
+            # then the number is a 2 byte character
+            if ((num >> 5) == 0b110):
                 remaining_bytes = 1
+            # if the leftmost 4 bits are 1110,
+            # then the number is a 3 byte character
             elif (num >> 4) == 0b1110:
                 remaining_bytes = 2
+            # if the leftmost 5 bits are 11110,
+            # then the number is a 4 byte character
             elif (num >> 3) == 0b11110:
                 remaining_bytes = 3
             else:
                 return False
         else:
+            # if the two most significant bits are not 10,
+            # then the number is not a valid continuation byte
             if (num & 0b11000000) != 0b10000000:
                 return False
+
+            # decrement the number of remaining bytes if the number is a
+            # valid continuation byte
             remaining_bytes -= 1
 
+    # if the number of remaining bytes isn't 0, then the data set is incomplete
     return remaining_bytes == 0
