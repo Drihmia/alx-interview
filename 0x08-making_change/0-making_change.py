4fb447e99b34333069439d38cd3b64ad7e3d5886
Drihmia <drihmia.redouane@gmail.com>
2024-07-28 14:42:41 +0100

    fix: handle some edge cases, using simplest approach so far

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,17 @@ Making Change
 
 
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number of
+    coins needed to meet a given amount total.
+    """
+
+    if total <= 0:
+        return 0
+
+    if len(coins) == 0:
+        return -1
+
     number_coins = 0
     for coin in sorted(coins, reverse=True):
         while (total > 0):
@@ -13,4 +24,7 @@ def makeChange(coins, total):
                 total = total + coin
                 break
             number_coins += 1
+
+    if total != 0:
+        return -1
     return number_coins
