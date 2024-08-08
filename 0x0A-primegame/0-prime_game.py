944da0887a057d9fc87514c1e940ba1265dd762e
Drihmia <drihmia.redouane@gmail.com>
2024-08-08 03:07:04 +0100

    test5

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """Prime Game"""
-from math import sqrt
 
 
 def is_prime(x):
-    if not x or x == 1:
+    """
+    x: an integer
+    Determine if x is a prime number
+    """
+    if x <= 1:
+        return False
+    if x <= 3:
+        return True
+
+    if x % 2 == 0 or x % 3 == 0:
         return False
 
-    for i in range(2, int(sqrt(x)) + 1):
+    for i in range(5, x**0,5 + 1):
         if x % i == 0:
             return False
 
@@ -19,6 +27,9 @@ def round_winner(n):
     n: an integer representing the number of integers
     Determine the winner of the round using the Sieve of Eratosthenes
     """
+    if not n:
+        return None
+
     ints = list(range(2, n + 1))
     if not ints:
         return 'Ben'
@@ -55,12 +66,22 @@ def isWinner(x, nums):
     x: an integer representing the number of rounds
     nums: an array of n integers
     """
+    if len(nums) != x:
+        return None
+
+    if not x or not nums:
+        return None
+
+    if x == 1 and nums[0] == 0:
+        return None
+
     maria_score = 0
     ben_score = 0
+
     for n in nums:
         if round_winner(n) == 'Maria':
             maria_score += 1
-        else:
+        elif round_winner(n) == 'Ben':
             ben_score += 1
 
     # Decision is made
